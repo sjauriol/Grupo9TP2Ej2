171fff5b108d12198cf681044d397c308ab69f13
Jessica Auriol <21082656+sjauriol@users.noreply.github.com>
2026-09-01 19:40:28 -0300

    Primer commit

---
+++ src/main/java/com/prog/grupo9tp2ej2/Grupo9TP2Ej2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.prog.grupo9tp2ej2;
+
+/**
+ *
+ * @author usuario
+ */
+public class Grupo9TP2Ej2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
